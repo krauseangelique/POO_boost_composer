276e10133bb2aa54c39a9abb31de7fcaa807668f
angelique KRAUSE <krause_angelique@hotmail.com>
2023-10-30 18:16:36 +0100

    add init index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+//use Isl\BoostComposer\AdresseManager;
+
+// Utilisez la méthode d’autoload  de composer pour charger automatiquement les classes de cet exercice
+//require_once 'vendor/autoload.php';
+echo "Hello ma base de données test_poo_adresse_boost";
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicesPOOAdresse_Boost_Composer</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
